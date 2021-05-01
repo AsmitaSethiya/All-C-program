20182e915c9fd047e544c9474e4c19b210fed314
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-05-02 00:40:31 +0530

    Create function_oddno.c

---
+++ function_oddno.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void get_OddNumber(int[], int);
+
+int main()
+{
+    int numbers[10] = {2, 98, 99, 27, 31, 53, 88, 92, 11, 12};
+
+    get_OddNumber( numbers, 10);
+   
+
+    return 0;
+
+}
