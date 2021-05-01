a1d609622edf03e4f26702929f427d0fc667f3a9
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-05-02 00:22:01 +0530

    Create naturalno.c

---
+++ naturalno.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int counter = 1;
+
+    while ( counter <= 10)
+    {
+        printf("%d\n", counter);
+        counter++;
+    }  
+
+    return 0;
+ 
+}
+© 2021 GitHub, Inc.
