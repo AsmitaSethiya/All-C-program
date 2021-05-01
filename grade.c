75ad973c61a66eee1bc20fb96dd098f03c5a6f13
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-05-02 00:11:31 +0530

    Update grade.c

--- grade.c
+++ grade.c
@@ -1,4 +1,25 @@
 // If else ladder concept in c
+/*
+In this program we write code using c language for generate student grade depending on the student marks.
+
+Maximum marks that can be secured by a student is 500
+
+If marks secured by the student is :
+
+Greater than or equal to 450 and less than or equal to 500: ‘A’ grade
+
+Greater than or equal to 400 and below 450: ‘B’ grade
+
+Greater than or equal to 350 and below 400: ‘C’ grade
+
+Greater than or equal to 250 and below 350: ‘D’ grade
+
+Greater than or equal to 150 and below 250: ‘E’ grade
+
+Less than 150: ‘F’ grade
+
+Allow user to input his total marks secured out of 500
+*/
 #include<stdio.h>
 int main()
 {
